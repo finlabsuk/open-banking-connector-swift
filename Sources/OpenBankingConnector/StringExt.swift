564fd599e55d8c561b2609fdfbb28f30c9af97de
Mark Murphy <mark@finnovationlabs.com>
2019-09-03 14:46:12 +0100

    Code drop 2.

---
+++ Sources/OpenBankingConnector/StringExt.swift
@@ -0,0 +1,17 @@
+// ********************************************************************************
+//
+// This source file is part of the Open Banking Connector open source project
+//
+// Copyright (C) 2019 Finnovation Labs and the Open Banking Connector project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// ********************************************************************************
+
+import Foundation
+
+/// Enable throwing string as an error
+extension String: Error {}
